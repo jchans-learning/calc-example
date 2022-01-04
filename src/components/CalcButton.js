f0377396046f6f5ef84b898af2265328f43dec33
jchans <jiangchenghans@gmail.com>
2022-01-04 10:40:36 +0800

    整理程式碼： mathjs 處理字串已經可以避免 00 開頭數字的錯誤，移除判斷 00 開頭的判斷。

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -18,15 +18,6 @@ function CalcButton(props) {
         className="bg-gray-500 focus:bg-gray-700 text-white font-bold py-2 rounded md-css-adjust md:w-11"
         onClick={() => {
           if (isError) return;
-          // 避免 00 開頭的數字產生的錯誤。
-          if (
-            (inputNumStr + buttonNumStr).slice(0, 1) === "0" &&
-            (inputNumStr + buttonNumStr).slice(1, 2) !== "." &&
-            (inputNumStr + buttonNumStr).slice(1, 2) === "0"
-          ) {
-            setInputNumStr("0");
-            return;
-          }
 
           // 在運算後，如果沒有按運算紐直接按數字，預設使用上一次使用的運算子
           if (isProcessed) {
